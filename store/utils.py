60331b2478bc8e59d3b9be4c34b704b89293c7ed
EvgenyS <e-stolpovski@mail.ru>
2023-03-15 19:55:42 +0300

    add Домашнее задание 15.1

--- store/utils.py
+++ store/utils.py
@@ -52,7 +52,41 @@ class Item:
 
     def __str__(self):
         return f'{self.name}'
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.count + other.count
+        else:
+            raise ValueError("Сложение с другими аргументами класса запрещено")
+
+class Phone(Item):
+    """Класс телефон, наследственный класс"""
+    def __init__(self, name, price, count, sim_card):
+        """Инициализация нового атрубута- количество сим-карт"""
+        super().__init__(name, price, count)
+        self.sim_card = sim_card
+        if sim_card == 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        else:
+            self.sim_card = sim_card
+
+# смартфон iPhone 14, цена 120_000, количетсво товара 5, симкарт 2
+phone1 = Phone("iPhone 14", 120_000, 5, 2)
+print(phone1)
+#iPhone 14
+
+print(repr(phone1))
+#Phone('iPhone 14', 120000, 5, 2)
+
+#phone1.number_of_sim = 0
+#ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.
+
 
 item1 = Item("Смартфон", 10_000, 20)
-print(repr(item1)) # Смартфон, 10000, 20
-print(item1) # Смартфон
+#print(repr(item1)) # Смартфон, 10000, 20
+#print(item1) # Смартфон
+
+
+
+
+
+
